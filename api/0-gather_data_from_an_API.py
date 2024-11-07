9deea2f9892dbcaf23e9050ac5715e7562d8798c
Nash Thames <taylor.thames@atlasschool.com>
2024-11-07 00:35:57 -0600

    0.1 - errors for fetch fails

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,3 +14,14 @@ def fetch_employee_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com/"
     user_url = f"{base_url}users/{employee_id}"
     todo_url = f"{base_url}todos?userId={employee_id}"
+
+    todo_response = requests.get(todo_url)
+    user_response = requests.get(user_url)
+
+    # check for valid responses
+    if todo_response.status_code != 200:
+        print(f"To-Do List Fetch Fail: {employee_id}")
+        return
+    if user_response.status_code != 200:
+        print(f"Name Fetch Fail: {employee_id}")
+        return
